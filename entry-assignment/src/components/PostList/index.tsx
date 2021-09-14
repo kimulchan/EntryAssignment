9077f47b499de1009a012aea851c85d86a39e4e4
kimulchan <leerabi577@gmail.com>
2021-09-14 23:16:56 +0900

    ⚡::PostList 퍼블리싱

---
+++ entry-assignment/src/components/PostList/index.tsx
@@ -0,0 +1,16 @@
+import * as B from "./style";
+
+function PostList (){
+    return(
+        <B.Wrapper>
+            <B.ListWrapper>
+                <B.Head>
+                    <B.HNumberBox>NO</B.HNumberBox>
+                    <B.HTitleBox>제목</B.HTitleBox>
+                </B.Head>
+            </B.ListWrapper>
+        </B.Wrapper>
+    );
+}
+
+export default PostList;
---
+++ entry-assignment/src/components/PostList/style.ts
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    padding:105px 320px 0 320px;
+    display:flex;
+    flex-direction: column;
+    background-color:#fdfdfd;
+`
+export const ListWrapper = styled.div`
+    height:495px;
+    display:flex;
+    flex-direction: column;
+`
+export const Head = styled.div`
+    height:45px;
+    border-top: 2px solid #000000;
+    border-bottom: 2px solid #000000;
+    display:flex;
+    align-items:center;
+`
+export const HNumberBox =styled.div`
+    margin-left :50px ;
+    font-size:14px;
+` 
+export const HTitleBox = styled.div`
+    margin-left: 350px;
+    font-size: 14px;
+`
