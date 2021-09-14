91756a34cd4a8cfd9be68e5bfbcb09c02753fac3
kimulchan <leerabi577@gmail.com>
2021-09-14 23:17:37 +0900

    ⚡::UserApi 기능 추가

---
+++ entry-assignment/src/utils/api/user.ts
@@ -0,0 +1,33 @@
+import request from "../axios/axios";
+
+export default {
+    sign(id:string,password:string){
+        return request({
+            url:"/account",
+            method:"post",
+            data:{
+                id:id,
+                password:password
+            }
+        })
+    },
+    postAuth(password:string,userId:string){
+        return request({
+            url:"/auth",
+            method:"post",
+            data:{
+                password:password,
+                userId:userId
+            }
+        })
+    },
+    getMypage(){
+        return request({
+            url:"/mypage",
+            method:"get",
+            headers:{
+                Authorization : `Bearer ${localStorage.accessToken}`
+            }
+        })
+    }
+}
