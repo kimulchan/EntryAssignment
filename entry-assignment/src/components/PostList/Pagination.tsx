6c50122a85ff6e99148c0c5604e1093cedd23423
kimulchan <leerabi577@gmail.com>
2021-09-16 22:49:57 +0900

    ⚡:: 페이지네이션 기능 추가

---
+++ entry-assignment/src/components/PostList/Pagination.tsx
@@ -0,0 +1,49 @@
+import { useEffect } from "react";
+import * as s from "./style";
+interface PaginationProps{
+    postPerPage:number,
+    totalPosts:number,
+    paginate:any,
+    currentPage:number
+}
+
+function Pagination({postPerPage,totalPosts,paginate,currentPage}:PaginationProps){
+    
+    
+    const pageNumbers:number[] =[];
+    
+    for(let i=1; i<=Math.ceil(totalPosts/postPerPage); i++){
+        pageNumbers.push(i);
+    }
+    
+    const PageNumMap = (props:number)=>{
+        return(
+            <s.PageNum key={props} currentPage={currentPage} onClick={()=>{  
+                paginate(props);
+
+            }}>{props}</s.PageNum>
+        );
+    }
+    return (
+        <s.PageNumBox>
+            <s.LSign onClick={()=>{
+                if(currentPage===pageNumbers[0]){
+                    alert("더 이상 넘어갈 수 없습니다")
+                    return;
+                }
+                paginate(currentPage-1)
+            }} ></s.LSign>
+            {pageNumbers.map(PageNumMap)}
+            <s.RSign onClick={()=>{
+                if(currentPage===pageNumbers[pageNumbers.length-1]){
+                    alert("더 이상 넘어갈 수 없습니다")
+                    return;
+                }
+                paginate(currentPage+1)
+
+            }} ></s.RSign>
+        </s.PageNumBox>
+    )
+}
+
+export default Pagination;
