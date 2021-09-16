627d3478f2ebac5c35d0f7e45aa18b0bd66f49aa
kimulchan <leerabi577@gmail.com>
2021-09-16 22:47:48 +0900

    ⚡:: writpost 와 updatepost ,postshow 라우터 구현을 위한 번들

--- entry-assignment/src/components/index.ts
+++ entry-assignment/src/components/index.ts
@@ -1,3 +1,6 @@
 export {default as Header} from "./Header";
 export {default as BoardList} from "./PostList";
 export {default as Auth} from "./Auth";
+export {default as WritePost} from "./WritePost/WritePost";
+export {default as UpdatePost} from "./WritePost/UpdatePost"
+export {default as PostShow} from "./PostShow";
