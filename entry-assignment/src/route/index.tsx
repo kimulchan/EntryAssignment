caf59e29a15228630c86e084199bbbccdce33383
kimulchan <leerabi577@gmail.com>
2021-09-16 23:21:53 +0900

    ⚡:: route 기능 추가

--- entry-assignment/src/route/index.tsx
+++ entry-assignment/src/route/index.tsx
@@ -1,15 +1,22 @@
+import { useContext } from 'react';
 import { BrowserRouter, Route, Switch }from 'react-router-dom';
 import * as C from "../components";
-function route() {
+import { ModalContext } from '../context/modalContext';
+function Routes() {
+    const MContext:any = useContext(ModalContext);
     return (
         <BrowserRouter>
-            <C.Auth></C.Auth>
+            {(MContext.state.isLogin||MContext.state.isSignUp) && <C.Auth></C.Auth>}
             <C.Header></C.Header>
             <Switch>
-                <Route component={C.BoardList} path="/"></Route>
+                <Route component={C.BoardList} path="/" exact></Route>
+                <Route component={C.BoardList} path="/mypage" ></Route>
+                <Route component={C.WritePost} path="/writepost"></Route>
+                <Route component={C.UpdatePost} path="/updatepost/:id"></Route>
+                <Route component={C.PostShow} path="/postshow/:id"></Route>
             </Switch>
         </BrowserRouter>
     );
 }
 
-export default route;
+export default Routes;
